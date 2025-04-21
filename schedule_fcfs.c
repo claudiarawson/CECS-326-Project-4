92d4ec69cd4cc607b1960438f4635730723b3ad7
ReichenBrown <reichenbrown@live.com>
2025-04-20 19:58:29 -0700

    fcfs

--- schedule_fcfs.c
+++ schedule_fcfs.c
@@ -10,3 +10,46 @@
 #include "cpu.h"
 
 //Your code and design here
+
+// Global head of the list
+struct node *head = NULL;
+
+// Function to add task (called by driver.c)
+void add(char *name, int priority, int burst) {
+    static int tid = 1;  // Unique task ID
+
+    Task *newTask = malloc(sizeof(Task));
+    newTask->name = name;
+    newTask->priority = priority;
+    newTask->burst = burst;
+    newTask->tid = tid++;
+
+    insert(&head, newTask);
+}
+
+// Reverse the linked list (due to list.c implementation)
+struct node* reverseList(struct node* head) {
+    struct node* prev = NULL;
+    struct node* curr = head;
+    struct node* next = NULL;
+
+    while (curr != NULL) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+
+    return prev;
+}
+
+// FCFS Scheduling
+void schedule() {
+    head = reverseList(head); // reverse the list to work with list.c implementation
+
+    struct node *temp = head;
+    while (temp != NULL) {
+        run(temp->task, temp->task->burst);
+        temp = temp->next;
+    }
+}
