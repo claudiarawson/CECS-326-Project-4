d0db3a74f23635faaa5aa6f71b44878064cad9fb
Claudia Rawson <claudiarawson2021@gmail.com>
2025-04-20 20:52:37 -0700

    Priority Complete

--- schedule_priority.c
+++ schedule_priority.c
@@ -9,6 +9,34 @@
 #include "list.h"
 #include "cpu.h"
 
-/*
- * Your code and design here:
- */
+struct node *head = NULL;
+
+void add(char *name, int priority, int burst) {
+    static int tid = 1;  // Unique task ID
+
+    Task *newTask = malloc(sizeof(Task));
+    newTask->name = name;
+    newTask->priority = priority;
+    newTask->burst = burst;
+    newTask->tid = tid++;
+
+    insert(&head, newTask);
+}
+
+void schedule() {
+    while (head != NULL) {
+        struct node *highest = head;
+        struct node *current = head;
+
+        while (current != NULL) {
+            if (current->task->priority < highest->task->priority) {
+                highest = current;
+            }
+            current = current->next;
+        }
+
+        Task *selected = highest->task;
+        run(selected, selected->burst);
+        delete(&head, selected);
+    }
+}
